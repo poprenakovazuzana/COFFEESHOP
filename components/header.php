6a07e29eb85cabebf2aac883d7efb576d8fb4148
Ajmir Raziqi <ajmir.ng3@gmail.com>
2026-01-19 21:59:58 +0200

    fix ulohy page url in header.php

--- components/header.php
+++ components/header.php
@@ -2,7 +2,7 @@
 
 <header class="">
     <div class="header-left">
-        <img src="/icons/logo1.svg" ; alt="Logo" class="logo">
+        <img src="/icons/logo1.svg"  alt="Logo" class="logo">
         <h1 class="brand">COFFEE <br>FOR YOU</h1>
     </div>
     <nav>
@@ -10,6 +10,6 @@
         <a href="/menu.php">Menu</a>
         <a href="/objednavka.php">Objednajte si</a>
         <a href="/kontakty.php">Kontakty</a>
-        <a href="/Úlohy.php">Úlohy</a>
+        <a href="/ulohy.php">Úlohy</a>
     </nav>
 </header> 
